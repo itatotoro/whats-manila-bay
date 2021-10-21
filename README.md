aef3fc6ed447188d90d090338c5b4ece39382701
Glitch (whats-a-bay) <none>
2021-10-21 14:30:19 +0000

    🚵💰 Updated with Glitch

--- README.md
+++
@@ -1,2 +0,0 @@
-# whats-manila-bay
-This is a project for Independent Study: People and Planet (2021).
--- script.js
+++ script.js
@@ -37,20 +37,23 @@ document.getElementById("refs").innerHTML = "";
 */
 
 function detail(a){
-  if (r.matches){
-    document.getElementById("lcolumn").style.width = "100%";
-    document.getElementById("rcolumn").style.width = "100%";
-  }
-  else {
-    document.getElementById("lcolumn").style.width = "70%";
-    document.getElementById("rcolumn").style.width = "30%";
-  }
+  document.getElementById("rcolumn").style.display = "block";
   document.getElementById("rcolumn").style.opacity = 1;
   document.getElementById("grid").style.display = "none";
   document.getElementById("frame").style.display = "block";
   document.getElementById("frame").style.opacity = 1;
   document.getElementById("verify").style.opacity = 0;
   document.getElementById("row").style.margin = 0;
+  
+  if (r.matches){
+    document.getElementById("lcolumn").style.width = "100%";
+    document.getElementById("rcolumn").style.width = "100%";
+    document.getElementById("frame").style.height = "400px";
+  }
+  else {
+    document.getElementById("lcolumn").style.width = "70%";
+    document.getElementById("rcolumn").style.width = "30%";
+  }
   
   if (a == 1){
     document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2Fmangroves.jpg?v=1633153658313)";
@@ -79,24 +82,45 @@ function detail(a){
     document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2Fgen2-manila-bay-sunset_2019-01-26_22-42-15.jpg?v=1633159016184)";
   }
   if (a == 6){
-    document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2FBlack-faced_spoonbill-e1579512631662-1840x500.jpg?v=1633159015984)";
-    document.getElementById("frame").style.backgroundSize = "contain";
-    document.getElementById("frame").style.backgroundRepeat = "no-repeat";
+    if (r.matches) {
+      document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2Fbird%20crop.jpg?v=1633160564961)";
+      document.getElementById("frame").style.backgroundSize = "cover";
+    }else{
+      document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2FBlack-faced_spoonbill-e1579512631662-1840x500.jpg?v=1633159015984)";
+      document.getElementById("frame").style.backgroundSize = "contain";
+      document.getElementById("frame").style.backgroundRepeat = "no-repeat";
+    }
     document.getElementById("picinfo").innerHTML ='<b>Black-faced Spoonbill</b><br><i>Platelea minor</i><ul type="disc"><li>Globally endangered</li><li>Originates from mainland China and North Korea, migrates south in the winter</li><li>24 found resting, the highest number reported for the bird in the country</li><li>Before 2019, they were not recorded in Manila Bay for over a century</li><li>Like other migratory birds, good indicator for ecosystem health</li></ul>';
     document.getElementById("refs").innerHTML ='January 11, 2020 | Mudflat areas of Barangay Taliptip in Bulacan<br><a href="https://www.wetlands.org/news/largest-ever-number-of-endangered-black-faced-spoonbills-recorded-in-the-philippines/">Photo:</a> Jasmin Meren<br>References: <a href ="https://businessmirror.com.ph/2020/01/14/globally-endangered-black-faced-spoonbills-recorded-in-bulacan/">[1]</a> <a href = "https://globalnation.inquirer.net/2289/manila-bay-bird-sanctuary-under-threat">[2]</a>';
   }
   if (a == 7){
-    document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2FTaliptip%20chapel.jpg?v=1633159109434)";
+    if (r.matches) {
+      document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.me/fb10f383-0890-409e-b36f-cef479b16ff7%2FTaliptip%20crop.jpg?v=1634825191264)";
+      document.getElementById("frame").style.backgroundSize = "cover";
+    }else{
+      document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2FTaliptip%20chapel.jpg?v=1633159109434)";
+    }
     document.getElementById("picinfo").innerHTML ='Translated quotes from former residents of Barangay Taliptip, site of San Miguel Corporation’s New Manila International Airport (construction ongoing) <sup><a href="https://www.philstar.com/headlines/2020/10/14/2049252/taliptip-long-goodbye-where-sea-will-soon-meet-sky">[1]</a></sup><br><br><ul style="disc"><li>“We have had weddings there, and baptisms.” - Thelma Bantigue, 38</li><li>"It&#39;s different if you work for someone. They&#39;ll shout at you if you make a mistake… All my husband and I know are the sea.” - Anita Anastacio, 39</li><li>"Maybe this is as far as we can go and we just have to accept it. We fought it but we lost. We can meet them with our heads held high. We can look them in the eye [because] we stood up for our rights." Teodoro Bacon, 42</li></ul>';
     document.getElementById("refs").innerHTML ='October 2020 | Abandoned chapel on a sandbar called “Torres”, Barangay Taliptip, Bulakan, Bulacan<br><a href="https://www.philstar.com/headlines/2020/10/14/2049252/taliptip-long-goodbye-where-sea-will-soon-meet-sky">Photo</a>: Efigenio Toledo IV | Philippine Star';
+    
   }
   if (a == 8){
-    document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2FManila%20Bay%20swimming.jpg?v=1633159109773)";
+     if (r.matches) {
+      document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2Fswim%20crop.jpg?v=1633160720913)";
+      document.getElementById("frame").style.backgroundSize = "cover";
+    }else{
+      document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2FManila%20Bay%20swimming.jpg?v=1633159109773)";
+    } 
     document.getElementById("picinfo").innerHTML ='<b>What&#39;s in this water?</b><br><br><ul style="disc"><li>Fecal coliform bacteria in the water [2019: 1.26 billion MPN/100mL, standard is 100 MPN/100mL]<sup><a href="https://www.pna.gov.ph/articles/1151877">[1]</a></sup></li><li>Household wastes dumped into the 25-kilometer long Pasig River, plus other rivers and estuaries<sup><a href="https://www.rappler.com/newsbreak/in-depth/why-manila-bay-cannot-be-saved-6-months-rehabilitation-series-part-2">[2]</a></sup></li><li>Wastewater discharge from waterside establishments</li><li>85% of Metro Manila’s household toilets, unconnected to sewerage system<sup><a href="https://mb.com.ph/2021/02/18/a-reminder-on-the-problem-of-pollution-in-manila-bay/">[3]</a></sup></li><li>Single-use plastics, among other kinds of nonbiodegradables<sup><a href="https://www.pna.gov.ph/articles/1063387">[3]</a> <a href="https://www.rappler.com/environment/plastic-bags-garbage-manila-bay">[4]</a></sup></li></ul>Swimming in Manila Bay is banned by Manila ordinance.';
     document.getElementById("refs").innerHTML =' June 8, 2018 | Strip of sand at the stalled Baseco Reclamation Expansion Project<br><a href="https://www.rappler.com/nation/manila-bay-world-ocean-day-photos">Photo: </a> Leanne Jazul | Rappler<br>References: <a href="https://www.rappler.com/nation/manila-bay-lgus-violating-environmental-laws">[5]</a> <a href="https://www.cnnphilippines.com/news/2020/1/23/denr-manila-bay-rehab-fecal-coliform-levels.html">[6]</a>';
   }
   if (a == 9){
-    document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2FFisherfolk%20protest.jfif?v=1633159109329)";
+    if (r.matches) {
+      document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.me/fb10f383-0890-409e-b36f-cef479b16ff7%2FFisherfolk-protest-c.jpg?v=1634825546265)";
+      document.getElementById("frame").style.backgroundSize = "cover";
+    }else{
+      document.getElementById("frame").style.backgroundImage = "url(https://cdn.glitch.com/fb10f383-0890-409e-b36f-cef479b16ff7%2FFisherfolk%20protest.jfif?v=1633159109329)";
+    } 
     document.getElementById("picinfo").innerHTML ='Protest signage (translated from Filipino):<br><br>Defend livelihood <br>Demolition of mussel farms and fish pens<br>Oppose, resist, do not permit';
     document.getElementById("refs").innerHTML ='September 7, 2021 | Protest by the Samahan ng Mangingisda sa Parañaque (PANGISDA-Pilipinas) in La Huerta, Parañaque to stop demolition of fishpens in DENR’s Manila Bay rehabilitation project<br><a href="https://news.abs-cbn.com/news/multimedia/slideshow/09/07/21/the-day-in-photos-september-7-2021">Photo: </a>George Calvelo | ABS-CBN NewsReferences: <a href="https://news.abs-cbn.com/news/09/02/21/paggiba-ng-mga-tahongan-sa-manila-bay-tinutulan">[1]</a> <a href="https://angpamalakaya.org/2021/09/06/pamalakayas-tahong-and-talaba-growers-storm-denr-ahead-of-its-demolition-order/">[2]</a>';
   }
